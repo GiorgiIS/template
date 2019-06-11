57fb8b6e6684de907bc013be7f40b2640b3a75ee
gisaiashvili <gisaiashvili@aldagi.ge>
2019-06-11 11:24:07 +0400

    added ef core

---
+++ ProjectTemplate.Repository.EF/CustomDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectTemplate.Repository.EF
+{
+    public class CustomDbContext : DbContext
+    {
+        public CustomDbContext(DbContextOptions<CustomDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
