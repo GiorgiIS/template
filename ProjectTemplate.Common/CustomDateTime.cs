95b7d4dd282d928c7e030285dc8ea9827c82c83c
gisaiashvili <gisaiashvili@aldagi.ge>
2019-06-11 11:16:21 +0400

    added custom datetime

---
+++ ProjectTemplate.Common/CustomDateTime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectTemplate.Common
+{
+    public class CustomDateTime
+    {
+        public static DateTime Now()
+        {
+            return DateTime.Now;
+        }
+    }
+}
