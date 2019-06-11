3b6e129413728a4366f3a93a9d53296828d2dac7
gisaiashvili <gisaiashvili@aldagi.ge>
2019-06-11 11:19:49 +0400

    added base entity

---
+++ ProjectTemplate.Core/Entities/BaseEntity.cs
@@ -0,0 +1,22 @@
+﻿using ProjectTemplate.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectTemplate.Core.Entities
+{
+    public abstract class BaseEntity
+    {
+        public BaseEntity()
+        {
+            CreatedAt = CustomDateTime.Now();
+            UpdatedAt = CustomDateTime.Now();
+            Id = Guid.NewGuid().ToString();
+        }
+
+        public string Id { get; set; }
+        public DateTime CreatedAt { get; }
+        public DateTime UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
