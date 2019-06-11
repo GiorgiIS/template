f760e58aba9ce18175ec17444c2c2733ae2f0cef
gisaiashvili <gisaiashvili@aldagi.ge>
2019-06-11 11:29:58 +0400

    added base repository

---
+++ ProjectTemplate.Repository/Implementations/Repository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectTemplate.Repository.EF;
+using ProjectTemplate.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ProjectTemplate.Repository.Implementations
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected CustomDbContext RepositoryContext { get; set; }
+
+        public RepositoryBase(CustomDbContext repositoryContext)
+        {
+            this.RepositoryContext = repositoryContext;
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return RepositoryContext.Set<T>().AsNoTracking();
+        }
+
+        public IQueryable<T> Get(Expression<Func<T, bool>> expression)
+        {
+            return RepositoryContext.Set<T>().Where(expression).AsNoTracking();
+        }
+
+        public void Create(T entity)
+        {
+            RepositoryContext.Set<T>().Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            RepositoryContext.Set<T>().Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            RepositoryContext.Set<T>().Remove(entity);
+        }
+    }
+}
