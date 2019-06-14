61ffaea46660b655ffd115e3c070d9883713e735
gisaiashvili <gisaiashvili@aldagi.ge>
2019-06-14 15:57:26 +0400

    added crud controller

---
+++ ProjectTemplate.Common.Api/CrudController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectTemplate.Core.Entities;
+using ProjectTemplate.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ProjectTemplate.Common.Api
+{
+    public abstract class CrudController<Dto, DbModel> : ControllerBase where DbModel : BaseEntity
+    {
+        protected readonly IRepositoryBase<DbModel> _repository;
+
+        public CrudController(IRepositoryBase<DbModel> repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpPost]
+        public virtual Guid Create([Required][FromBody] Dto input)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public virtual Dto Get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public virtual IEnumerable<Dto> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public virtual void Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public virtual void Update(Dto input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
