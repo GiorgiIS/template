30c0f5cdef88bf24549570caa5f35ee7c1d51d00
gisaiashvili <gisaiashvili@aldagi.ge>
2019-07-11 18:39:54 +0400

    fixed namespace bug

--- ProjectTemplate.Services/Interfaces/ICrudService.cs
+++ ProjectTemplate.Services/Interfaces/ICrudService.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace ProjectTemplate.Services.Interfaces
+namespace ProjectTemplate.Interfaces.Services
 {
     public interface ICrudService<T>
     {
